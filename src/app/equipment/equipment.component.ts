87e965553b1aff52f4e6978fb5286cf123bea60b
Emily Williams <emilydkoester@gmail.com>
2020-11-04 20:11:50 -0600

    completed bonus missions 2-7

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -9,7 +9,25 @@ export class EquipmentComponent implements OnInit {
 
   constructor() { }
 
+  equipment: string[] = ['Habitat dome', 'Drones', 'Food containers', 'Oxygen tanks'];
+  itemBeingEdited: string = null;
+
   ngOnInit() {
+  }
+  add(itemName: string) {
+    this.equipment.push(itemName);
+  }
+  remove(item: string) {
+    let index = this.equipment.indexOf(item);
+    this.equipment.splice(index, 1);
+  }
+  edit(item: string) {
+    this.itemBeingEdited = item;
+  }
+  save(item: string) {
+    let index = this.equipment.indexOf(this.itemBeingEdited)
+    this.equipment[index] = item;
+    this.itemBeingEdited = null;
   }
 
 }
--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
 
+  experiments: string[] = ['Mars soil sample', 'Plant growth in habitat', 'Human bone density'];
+  experimentBeingEdited: string = null;
+
   constructor() { }
 
   ngOnInit() {
+  }
+  add(item: string) {
+    this.experiments.push(item);
+  }
+  remove(item: string) {
+    let index = this.experiments.indexOf(item);
+    this.experiments.splice(index, 1);
+  }
+  edit(item: string) {
+    this.experimentBeingEdited = item;
+  }
+  save(item: string) {
+    let index = this.experiments.indexOf(this.experimentBeingEdited)
+    this.experiments[index] = item;
+    this.experimentBeingEdited = null;
   }
 
 }
